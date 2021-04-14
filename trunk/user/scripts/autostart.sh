0f7fb8392f9c8fec77ea6f9c2ac9ce79c5faaa16
lemontreenm <61805387+lemontreenm@users.noreply.github.com>
2021-04-14 14:42:52 +0800

    Update autostart.sh
    调整插件启动顺序，adbyby调整至最后以免文件管理无法启动

--- trunk/user/scripts/autostart.sh
+++ trunk/user/scripts/autostart.sh
@@ -33,11 +33,6 @@ do
 	fi
 done
 
-if [ $(nvram get adbyby_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动adbyby plus+"
-/usr/bin/adbyby.sh start
-fi
-
 if [ $(nvram get koolproxy_enable) = 1 ] ; then
 logger -t "自动启动" "正在启动koolproxy"
 /usr/bin/koolproxy.sh start
@@ -46,6 +41,11 @@ fi
 if [ $(nvram get aliddns_enable) = 1 ] ; then
 logger -t "自动启动" "正在启动阿里ddns"
 /usr/bin/aliddns.sh start
+fi
+
+if [ $(nvram get zerotier_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动zerotier"
+/usr/bin/zerotier.sh start
 fi
 
 if [ $(nvram get ss_enable) = 1 ] ; then
@@ -58,17 +58,17 @@ logger -t "自动启动" "正在启动adguardhome"
 /usr/bin/adguardhome.sh start
 fi
 
-if [ $(nvram get wyy_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动音乐解锁"
-/usr/bin/unblockmusic.sh start
-fi
-
 if [ $(nvram get caddy_enable) = 1 ] ; then
 logger -t "自动启动" "正在启动文件管理"
 /usr/bin/caddy.sh start
 fi
 
-if [ $(nvram get zerotier_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动zerotier"
-/usr/bin/zerotier.sh start
+if [ $(nvram get wyy_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动音乐解锁"
+/usr/bin/unblockmusic.sh start
+fi
+
+if [ $(nvram get adbyby_enable) = 1 ] ; then
+logger -t "自动启动" "正在启动adbyby plus+"
+/usr/bin/adbyby.sh start
 fi
